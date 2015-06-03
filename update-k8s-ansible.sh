e8d7347e0f42be1c97cc5d7bc74c9e4f9495ddf3
Nick Coghlan <ncoghlan@gmail.com>
2015-06-03 15:32:10 +1000

    Locally modified Ansible playbook

--- update-k8s-ansible.sh
+++ update-k8s-ansible.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 # Updates the bundled Kubernetes Ansible scripts for the next image build
 git fetch https://github.com/eparis/kubernetes-ansible
-git subtree pull --prefix content/kubernetes-ansible \
+git subtree pull --prefix content/omv/ansible/modules/kubernetes \
                  https://github.com/eparis/kubernetes-ansible master --squash
